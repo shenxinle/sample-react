4ef26e7cd1e46c829f772ad989ba7e22d8784262
沈新乐 <shenxl@Ctrip.com>
2017-08-02 19:22:47 +0800

    go on

---
+++ src/demo/modal/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ModalDemo extends Component {
+    render() {
+        return <div>
+            <div>modal</div>
+        </div>
+    }
+}
+
+export default ModalDemo;
